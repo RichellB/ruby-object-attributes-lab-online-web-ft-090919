38e30cdaafca0d7ed607264abba22c0c09b43fe3
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-17 10:03:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,3 +12,14 @@ class Dog
   end
   
   
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end 
+  
+  def name 
+    @this_dog_breed
+  end 
+  
+  def breed=(new_breed)
+    @this_dog_breed = new_breed
+  end
