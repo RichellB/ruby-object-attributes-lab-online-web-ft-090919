dd07c30b0be215704c727287d1f3eaf1c278591f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-17 10:03:21 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def name=(dog_name)
+    @this_dog_name = dog_name 
+  end 
+  
+  def name 
+    @this_dog_name
+  end 
+  
+  def name=(new_name)
+    @this_dog_name = new_name
+  end
+  
+  
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end 
+  
+  def name 
+    @this_dog_breed
+  end 
+  
+  def breed=(new_breed)
+    @this_dog_breed = new_breed
+  end
