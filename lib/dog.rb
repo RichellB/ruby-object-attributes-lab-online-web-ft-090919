f99d6b48d6c4c41c2b986b315c5f675fa6012b61
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-17 10:07:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,15 @@
 class Dog 
-  def name(dog_name)
-    @this_dog_name = dog_name 
-  end 
+  
+   def name=(new_name)
+    @this_dog_name = new_name
+  end
+  
   
   def name 
     @this_dog_name
   end 
   
-  def name=(new_name)
-    @this_dog_name = new_name
-  end
-  
+ 
   
   def breed(dog_breed)
     @this_dog_breed = dog_breed
